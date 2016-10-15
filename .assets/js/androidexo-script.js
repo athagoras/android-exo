ee457016bbc227de9d014de70512714fd321a980
athagoras <phongphan007@gmail.com>
2016-10-15 21:54:40 +0700

    First pull!
    This is the first pull in android-exo

---
+++ .assets/js/androidexo-script.js
@@ -0,0 +1,167 @@
+var sections = $('.nav-activate-scrolling');
+var nav = $('nav');
+var footer = $('footer');
+var landing = $('#landing');
+var scrollTime = .75;
+var scrollDistance = 200;
+var isSend = false;
+
+$(document).ready(function(){
+	$('.button-collapse').sideNav({closeOnClick: true});
+	$(".dropdown-button").dropdown({hover: false});
+	$('.scrollspy').scrollSpy({scrollOffset: 64});
+
+	$('.statCard-wrapper').parent().css({
+		'display': '-webkit-flex',
+	    'display': '-ms-flexbox',
+	    'display': 'flex',
+		'flex-wrap': 'wrap'
+	});
+
+
+	$('#contactActivate').click(function(){
+		$(this).slideToggle('1000');
+		$('#contactActivate').parent().next().slideToggle('1000');
+	});
+
+	$('#contact-action button[name=delete]').click(function(){
+		if (isSend) {
+			$('#contact-data').slideToggle('1000');
+			$('#contact-progress').slideToggle('1000');
+			$('#contact-action button[name=submit]').removeAttr('disabled');
+			isSend = false;
+		} else {
+			$('#contactActivate').parent().next().slideToggle('1000');
+			$('#contactActivate').slideToggle('1000');
+		}
+	});
+
+	$('#contact-action button[name=submit]').click(function(){
+		isSend = true;
+		$(this).attr('disabled','disabled');
+		$('#contact-data').slideToggle('1000');
+		$('#contact-progress').slideToggle('1000');
+	});
+
+});
+
+$(window).on("mousewheel DOMMouseScroll", function(event){
+	event.preventDefault();
+
+	var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+	var scrollTop = $(this).scrollTop();
+	var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+	TweenMax.to($(this), scrollTime, {
+		scrollTo : { y: finalScroll, autoKill:true },
+		ease: Power1.easeOut,
+		overwrite: 5
+	});
+});
+
+$(window).on('resize load', function () {
+	var windowWidth = $(this).width();
+	var windowHeight = $(this).height();
+	$('#landing').css('height', windowHeight-nav.outerHeight());
+
+
+	$('.statCard-wrapper').parent().css({
+		'flex-direction' : function () {
+			if (windowWidth <= 600) {
+				return 'column';
+			} else {
+				return 'row';
+			}
+		},
+
+	});
+});
+
+$(window).on('scroll', function () {
+    var scrollPosTop = $(this).scrollTop() + $(this).height() * 0.75;
+	var scrollPosBottom = $(this).scrollTop() + $(this).height();
+	var landingPosTop = landing.offset().top;
+	var landingPosBottom = landingPosTop + landing.outerHeight();
+	var footerPosTop = footer.offset().top;
+	var footerPosBottom = footerPosTop + footer.outerHeight();
+	var isLandingPos = scrollPosTop >= landingPosTop && scrollPosTop <= landingPosBottom;
+	var isFooterPos = scrollPosTop >= footerPosTop && scrollPosTop <= footerPosBottom;
+
+    sections.each(function() {
+        var sectionsPosTop = $(this).offset().top - nav.outerHeight();
+        var sectionsPosBottom = sectionsPosTop + $(this).outerHeight();
+		var isSectionsPos = scrollPosTop >= sectionsPosTop && scrollPosTop <= sectionsPosBottom;
+
+		if (!isLandingPos && !isFooterPos) {
+			if (isSectionsPos) {
+				nav.find('ul.nav-list li').removeClass('active');
+				nav.find('ul.nav-list li a[href="#'+$(this).attr('id')+'"]').parent().addClass('active');
+			}
+		} else {
+			nav.find('ul.nav-list li').removeClass('active');
+		}
+    });
+
+	var firstAnimation = function(){
+    	$('.statCard').each(function(){
+         	$(this).delay(500).animate({
+				opacity: 1
+			},2000);
+		});
+  	};
+});
+
+function initMap() {
+	var coordinator = {lat: 13.7243922, lng: 100.5592442};
+	var map = new google.maps.Map(document.getElementById('location-map'), {
+		center: coordinator,
+		zoom: 17,
+		styles: [
+		  {
+			"featureType": "landscape.natural.terrain",
+			"elementType": "geometry",
+			"stylers": [ { "color": "#b2ff03" } ]
+		  },
+		  {
+			"featureType": "poi.park",
+			"elementType": "geometry",
+			"stylers": [ { "color": "#b2ff59" } ]
+			},
+		  {
+			"featureType": "road",
+			"elementType": "geometry",
+			"stylers": [ { "color": "#ffcdd2" } ]
+		  },
+		  {
+			"featureType": "road",
+			"elementType": "geometry.stroke",
+			"stylers": [ { "color": "#ffebee" }, { "weight": 1.5 } ]
+		  },
+		  {
+			"featureType": "road.arterial",
+			"elementType": "geometry",
+			"stylers": [ { "color": "#ff80ab" } ]
+		  },
+		  {
+			"featureType": "road.highway",
+			"elementType": "geometry",
+			"stylers": [ { "color": "#ef9a9a" } ]
+		  },
+		  {
+			"featureType": "road.highway.controlled_access",
+			"elementType": "geometry",
+			"stylers": [ { "color": "#e57373" } ]
+		  },
+		  {
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [ { "color": "#40c3ff" } ]
+		  }
+	  ]
+	});
+	var marker = new google.maps.Marker({
+		position: coordinator,
+		map: map,
+		title: 'ศูนย์ประชุมแห่งชาติสิริกิติ์ กรุงเทพมหานคร'
+	});
+}
